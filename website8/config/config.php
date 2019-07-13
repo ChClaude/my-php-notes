0463307ad5f1acc7ef7cf128013a6ec9f72718b8
Claude Christ <christ.tchambila@gmail.com>
2019-07-13 15:15:11 +0200

    Php blog post added

---
+++ website8/config/config.php
@@ -0,0 +1,6 @@
+<?php
+    define('ROOT_URL', 'http://localhost/LearnPHP/website8/');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'Cla@5027jesuschrist');
+    define('DB_NAME', 'phpblog');
